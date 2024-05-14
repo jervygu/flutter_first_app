2ca4aaf86b7de9c7754bf0bb36d4320330b2e2e7
Jervy Umandap <jegumandap@unionbankph.com>
2024-05-14 16:06:46 +0800

    ### Flutter Course
    * White Hello World Text in center with gradient background color.

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,56 +1,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-// With Flutter, you create user interfaces by combining "widgets"
-// You'll learn all about them (and much more) throughout this course!
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // Every custom widget must have a build() method
-  // It tells Flutter, which widgets make up your custom widget
-  // Again: You'll learn all about that throughout the course!
-  @override
-  Widget build(BuildContext context) {
-    // Below, a bunch of built-in widgets are used (provided by Flutter)
-    // They will be explained in the next sections
-    // In this course, you will, of course, not just use them a lot but
-    // also learn about many other widgets!
-    return MaterialApp(
-      title: 'Flutter First App',
-      theme: ThemeData(useMaterial3: true),
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
+        backgroundColor: Colors.deepPurple,
         body: Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              Text(
-                'Flutter - The Complete Guide Course - Jervy',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                ),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.amber,
+                Colors.deepPurple,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
               ),
-              SizedBox(height: 16),
-              Text(
-                'Learn Flutter step-by-step, from the ground up.',
-                textAlign: TextAlign.center,
-              ),
-            ],
+            ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
 
 
@@ -82,6 +60,107 @@ class MyApp extends StatelessWidget {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// // With Flutter, you create user interfaces by combining "widgets"
+// // You'll learn all about them (and much more) throughout this course!
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // Every custom widget must have a build() method
+//   // It tells Flutter, which widgets make up your custom widget
+//   // Again: You'll learn all about that throughout the course!
+//   @override
+//   Widget build(BuildContext context) {
+//     // Below, a bunch of built-in widgets are used (provided by Flutter)
+//     // They will be explained in the next sections
+//     // In this course, you will, of course, not just use them a lot but
+//     // also learn about many other widgets!
+//     return MaterialApp(
+//       title: 'Flutter First App',
+//       theme: ThemeData(useMaterial3: true),
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Welcome to Flutter'),
+//         ),
+//         body: Container(
+//           width: double.infinity,
+//           padding: const EdgeInsets.all(12),
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: const [
+//               Text(
+//                 'Flutter - The Complete Guide Course - Jerdvy',
+//                 textAlign: TextAlign.center,
+//                 style: TextStyle(
+//                   fontSize: 24,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               SizedBox(height: 16),
+//               Text(
+//                 'Learn Flutter step-by-step, from the ground up.',
+//                 textAlign: TextAlign.center,
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ================================================
 
 
 
